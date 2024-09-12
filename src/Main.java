6afe918fe94bc9f22584f50ac13b59a61c10d61f
Facundo Zumaeta <zumaetafacundo@gmail.com>
2024-09-12 17:14:11 -0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class Main {
@@ -47,49 +48,123 @@ public class Main {
             int suma = u + d + c;
             System.out.println("La suma de los 3 digitos es " + suma);
         }
-            /*-------------------------------------------------EJERCICIO 4-----------------------------------------------*/
-        if (ejercicios == 4){
+        /*-------------------------------------------------EJERCICIO 4-----------------------------------------------*/
+        if (ejercicios == 4) {
             System.out.println("Ingrese la cantidad a devolver");
-            float dev = sc.nextInt() ;
-            float doscientos = dev/200;
-            float res1= dev- (200*doscientos);
-            float cien= res1/100;
-            float res2= res1-(100*cien);
-            float cinc= res2/50;
-            float res3= res2-(50*cinc);
-            float veinte = res3/20;
-            float res4= res3-(20*veinte);
-            float cinco= res4/5;
-            float res5= res4 -(cinco*5);
-            float dos= res5/2;
-            float res6= res5 -(dos*2);
-            float uno=res6/1;
-            float res7=res6-(uno*1);
-            double cent=res7/0.05;
-            double res8= res7-(cent*0.05);
-            System.out.println("necesitas " +doscientos+ " billetes de doscientos,"+ "necesitas"+ cien + "billete de cien,"+ "necesitas " +cinc +" billete de cincuenta," + " necesitas" + veinte + " billetes de veinte,"+" necesitas"+ cinc+ " billetes de cinco,"+ "necesitas"+ dos+ " billetes de 2" + " necesitas "+ uno + " billetes de uno,"+ "y tambien necesitas " + cent + " monedas de 5 centavos");
+            float dev = sc.nextInt();
+            float doscientos = dev / 200;
+            float res1 = dev - (200 * doscientos);
+            float cien = res1 / 100;
+            float res2 = res1 - (100 * cien);
+            float cinc = res2 / 50;
+            float res3 = res2 - (50 * cinc);
+            float veinte = res3 / 20;
+            float res4 = res3 - (20 * veinte);
+            float cinco = res4 / 5;
+            float res5 = res4 - (cinco * 5);
+            float dos = res5 / 2;
+            float res6 = res5 - (dos * 2);
+            float uno = res6 / 1;
+            float res7 = res6 - (uno * 1);
+            double cent = res7 / 0.05;
+            double res8 = res7 - (cent * 0.05);
+            System.out.println("necesitas " + doscientos + " billetes de doscientos," + "necesitas" + cien + "billete de cien," + "necesitas " + cinc + " billete de cincuenta," + " necesitas" + veinte + " billetes de veinte," + " necesitas" + cinc + " billetes de cinco," + "necesitas" + dos + " billetes de 2" + " necesitas " + uno + " billetes de uno," + "y tambien necesitas " + cent + " monedas de 5 centavos");
         }
-            /*-------------------------------------------------EJERCICIO 5-----1------------------------------------------*/
+        /*-------------------------------------------------EJERCICIO 5-----1------------------------------------------*/
         if (ejercicios == 5) {
             System.out.print("Ingrese un numero");
             int numero = sc.nextInt();
             String numerostring = String.valueOf(numero);
             System.out.println(numerostring);
-                }
-        /*-------------------------------------------------EJERCICIO 6-----------------------------------------------*/
-        if (ejercicios == 6){
+        }
+        /*-------------------------------------------------EJERCICIO 6 & 9-----------------------------------------------*/
+        if (ejercicios == 6 || ejercicios== 9) {
             String mza = "La lluvia en Mendoza es escasa";
             int longitudmza = mza.length();
-            System.out.println("La longitud de la frase:La lluvia en Mendoza es escasa es " +longitudmza);
+            if (ejercicios == 6){
+            System.out.println("La longitud de la frase: La lluvia en Mendoza es escasa es " + longitudmza);}
+            if (ejercicios == 9){
+                int code= 0;
+                for (int i = 0; i<longitudmza; i++){
+                code = mza.codePointAt(i);
+                System.out.print(code+ " ");
+            }
+            }
         }
-        /*-------------------------------------------------EJERCICIO 7-----------------------------------------------*/
-       // Solicite el ingreso de una cadena y determine el tamaño de la misma y cuantas
-       //vocales tiene en total (recorre el String con charAt
-        if (ejercicios == 7)
+        /*-------------------------------------------------EJERCICIO 7 & 8-----------------------------------------------*/
+        // Solicite el ingreso de una cadena y determine el tamaño de la misma y cuantas
+        //vocales tiene en total (recorre el String con charAt
+        if (ejercicios == 7 || ejercicios == 8)
             System.out.println("Ingrese una cadena, se determinara el tamaño de esta y cuantas vocales tendra");
-            String cad= sc.nextLine();
+        String cad = sc.nextLine().toLowerCase();
+        if (ejercicios == 7) {
             int tam = cad.length();
-            System.out.println("El tamaño de la cadena es de "+ tam);
-        //no terminado
+            System.out.println("El tamaño de la cadena es de " + tam);
+            int i;
+            int vocales = 0;
+            for (i = 0; i < tam; i++) {
+
+                char a = cad.charAt(i);
+                if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') {
+                    vocales++;
+                }
+                ;
+            }
+            System.out.println("La cantidad de vocales es de " + vocales);
+        } else if (ejercicios == 8) {
+            String frase = cad;
+            for (int i = 0; i < cad.length(); i++) {
+            frase = cad.replace('a','e' );
+
+            }
+            System.out.println("ahora la frase quedaria como " + frase);
+        }
+        /*-------------------------------------------------EJERCICIO 10-----------------------------------------------*/
+    if (ejercicios== 10) {
+        System.out.println("Ingrese una frase");
+        String frase = sc.nextLine();
+        int opcion = 0;
+        while (opcion < 1 || opcion > 2) {
+            System.out.println("Ingrese si quiere convertir su frase en mayusculas o minusculas con 1 o 2");
+            opcion = Integer.parseInt(sc.nextLine());
+        }
+        ;
+        if (opcion == 1) {
+            System.out.println("Su frase en mayuscula es " + frase.toUpperCase());
+        }
+        if (opcion == 2) {
+            System.out.println("Su frase en mayuscula es " + frase.toLowerCase());
+        }
     }
+        /*-------------------------------------------------EJERCICIO 11-----------------------------------------------*/
+
+        if (ejercicios==11)
+        {System.out.println("Ingrese dos frases");
+            System.out.println("Primera frase");
+            String primera= sc.nextLine();
+            System.out.println("Segunda frase");
+            String segunda= sc.nextLine();
+            int a = primera.compareTo(segunda);
+            if (a>0){
+            System.out.println("Las frases son distintas" );
+            }
+            if(a==0) {
+                System.out.println("Las frases son iguales" );
+            }
+
+        }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
